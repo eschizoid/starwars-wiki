0ae29c06fe950bd53ce60300870c89a98f3b647a
Mariano Gonzalez <mariano.gonzalez@uptake.com>
2016-04-25 15:52:01 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict';
+module.change_code = 1;
+var _ = require('lodash');
+var Alexa = require('alexa-app');
+var StarWarsDataHelper = require('./star_wars_data_helper');
+var app = new Alexa.app('starwars-wiki');
+
+app.launch(function (req, res) {
+    var prompt = 'For Star Wars Character information, tell me name.';
+    res.say(prompt).reprompt(prompt).shouldEndSession(false);
+});
+
+app.intent('characterinfo', {
+        'slots': {
+            'NAME': 'CHARACTER'
+        },
+        'utterances': ['{|flight|airport} {|delay|status} {|info} {|for} {-|AIRPORTCODE}']
+    },
+    function (req, res) {
+        //get the slot
+        var name = req.slot('NAME');
+        var reprompt = 'Tell me a star wars character name to get their information.';
+        if (_.isEmpty(name)) {
+            var prompt = 'I didn\'t hear a star wars character name. Tell me a star wars character name.';
+            res.say(prompt).reprompt(reprompt).shouldEndSession(false);
+            return true;
+        } else {
+            var starWarsHelper = new StarWarsDataHelper();
+            starWarsHelper.requestCharacterInformation(name).then(function (character) {
+                console.log(name);
+                res.say(starWarsHelper.formatCharacterInformation(character)).send();
+            }).catch(function (err) {
+                console.log(err.statusCode);
+                var prompt = 'I didn\'t have data for character name ' + name;
+                //https://github.com/matt-kruse/alexa-app/blob/master/index.js#L171
+                res.say(prompt).reprompt(reprompt).shouldEndSession(false).send();
+            });
+            return false;
+        }
+    }
+);
+//hack to support custom utterances in utterance expansion string
+console.log(app.utterances().replace(/\{\-\|/g, '{'));
+module.exports = app;
---
+++ star_wars_data_helper.js
@@ -0,0 +1,29 @@
+'use strict';
+var _ = require('lodash');
+var swapi = require('swapi-node');
+
+function StarWarsDataHelper() {
+}
+
+StarWarsDataHelper.prototype.requestCharacterInformation = function (name) {
+    //TODO transform name into id
+    return swapi.getPerson(1).then(function (result) {
+        console.log(result);
+        return response.body;
+    });
+};
+
+StarWarsDataHelper.prototype.formatCharacterInformation = function (character) {
+    return _.template('name ${name}, height ${height}, mass ${mass}, hair color ${hairColor}, skin color ${skinColor}, eye color ${eyeColor}, birthYear ${birthYear}, gender ${gender}.')({
+        name: character.name,
+        height: character.height,
+        mass: character.mass,
+        hairColor: character.name,
+        skinColor: character.hair_color,
+        eyeColor: character.eye_color,
+        birthYear: character.birth_year,
+        gender: character.gender
+    });
+};
+
+module.exports = StarWarsDataHelper;
